a3f9f2fe4704494328ef014af3a2846fe40a82ac
nokdoot <nokdoot@naver.com>
2023-04-17 00:46:45 +0900

    change instruction file name

--- src/instruction-lib.rs
+++ src/instruction-lib.rs
@@ -11,29 +11,29 @@ use instruction::{IntroInstruction};
 
 entrypoint!(process_instruction);
 
-pub fn process_instruction(
+fn process_instruction(
     program_id: &Pubkey,
     accounts: &[AccountInfo],
     instruction_data: &[u8],
 ) -> ProgramResult {
     let instruction = IntroInstruction::unpack(instruction_data)?;
     match instruction {
-        IntroInstruction::InitUserInput {name, message} => {
+        IntroInstruction::InitUserInput {name, message}  => {
             add_student_intro(program_id, accounts, name, message)
         }
-    }
+    } 
 }
 
 pub fn add_student_intro(
-    program_id: &Pubkey,
-    accounts: &[AccountInfo],
-    name: String,
-    message: String,
+  program_id: &Pubkey,
+  accounts: &[AccountInfo],
+  name: String,
+  message: String,
 ) -> ProgramResult {
     msg!("Adding student intro...");
     msg!("Name: {}", name);
     msg!("Message: {}", message);
-
+    
     Ok(())
 
 }
